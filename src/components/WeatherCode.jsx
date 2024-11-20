eec80578f1c33bb720831ceaaf7bf5ee0efdf205
maxencedevweb <maxence.hipro@gmail.com>
2024-11-20 20:14:26 +0100

    Fix : Deplacements assets src -> public

--- src/components/WeatherCode.jsx
+++ src/components/WeatherCode.jsx
@@ -1,31 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import cloudsIcon from '../assets/img/clouds.png';
-import fogIcon from '../assets/img/fog.png';
-import heavyRainIcon from '../assets/img/heavy-rain.png';
-import partialSunIcon from '../assets/img/partial-sun.png';
-import slightSnowIcon from '../assets/img/slight-snow.png';
-import heavySnowIcon from '../assets/img/heavy-snow.png';
-import sunRainIcon from '../assets/img/sun-rain.png';
-import sunshineIcon from '../assets/img/sunshine.png';
-import thunderStormIcon from '../assets/img/thunderstorm.png';
 
 const WeatherCode = (props) => {
   const { code } = props;
   const weatherIcons = {
-    0: sunshineIcon,
-    2: partialSunIcon,
-    3: cloudsIcon,
-    45: fogIcon,
-    51: sunRainIcon,
-    65: heavyRainIcon,
-    71: slightSnowIcon,
-    75: heavySnowIcon,
-    80: heavyRainIcon,
-    85: heavySnowIcon,
-    95: thunderStormIcon,
+    0: '/assets/img/sunshine.png',
+    2: '/assets/img/partial-sun.png',
+    3: '/assets/img/clouds.png',
+    45: '/assets/img/fog.png',
+    51: '/assets/img/sun-rain.png',
+    65: '/assets/img/heavy-rain.png',
+    71: '/assets/img/slight-snow.png',
+    75: '/assets/img/heavy-snow.png',
+    80: '/assets/img/heavy-rain.png',
+    85: '/assets/img/heavy-snow.png',
+    95: '/assets/img/thunderstorm.png',
   };
-
   const determineIcon = (code) => {
     const icon = Object.keys(weatherIcons)
       .sort((a, b) => b - a)
